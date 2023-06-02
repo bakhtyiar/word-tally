bee95b7ca9c59706d65f2f3cef09b6a8aa3a29f9
fiendrsy <fiendrsy@PC.local>
2023-06-02 20:46:59 +0300

    code optimization

--- popup/main.js
+++ popup/main.js
@@ -1,8 +1,8 @@
 let minRepeats;
-let olElement = document.querySelector(".wordsList");
+let olElement = document.querySelector(".parsed-words__list");
 
 document.addEventListener("click", async e => {
-	if (e.target.classList.contains("parseButton")) {
+	if (e.target.classList.contains("parse-button")) {
 		await browser.tabs.executeScript(null, {
 			file: "../content-script/parse.js",
 		});
@@ -10,7 +10,7 @@ document.addEventListener("click", async e => {
 			active: true,
 			currentWindow: true,
 		});
-		minRepeats = document.querySelector(".filterCounters").value;
+		minRepeats = document.querySelector("#min-repeats__input").value;
 		olElement.textContent = "";
 		browser.tabs.sendMessage(activeTab[0].id, { minRepeats }, response => {
 			let popularWords = JSON.parse(response);
@@ -21,7 +21,7 @@ document.addEventListener("click", async e => {
 				olElement.append(liElement);
 			}
 		});
-	} else if (e.target.classList.contains("clear")) {
+	} else if (e.target.classList.contains("clear-button")) {
 		olElement.textContent = "";
 	}
 });
--- popup/tabs.js
+++ popup/tabs.js
@@ -1,6 +1,16 @@
-document.getElementById("defaultOpen").click();
+document.addEventListener("click", e => {
+	if (e.target.id === "filterTab") {
+		openTab(e, "filter-tab");
+		return;
+	} else if (e.target.id === "accountTab") {
+		openTab(e, "account-tab");
+		return;
+	}
+});
 
-function openTab(evt, tabName) {
+document.getElementById("filterTab").click();
+
+function openTab(e, tabName) {
 	let tabcontent, tablinks;
 	tabcontent = document.getElementsByClassName("tabcontent");
 	for (let i = 0; i < tabcontent.length; i++) {
@@ -11,5 +21,5 @@ function openTab(evt, tabName) {
 		tablinks[i].className = tablinks[i].className.replace(" active", "");
 	}
 	document.getElementById(tabName).style.display = "block";
-	evt.currentTarget.className += " active";
+	e.target.className += " active";
 }
